b05e9f8329a9b1b729306d1d4c332050e7e3bb0f
Hypekage <jmestmnag@gmail.com>
2023-09-17 20:24:59 +0545

    playing with language translator

--- apps/react-monorepo-practice/src/app/app.tsx
+++ apps/react-monorepo-practice/src/app/app.tsx
@@ -1,7 +1,15 @@
+import { useTranslation } from 'react-i18next';
 import './app.module.css';
 import { Form } from '@org/client/components/form';
 export function App() {
-  return <Form />;
+  const { t, i18n } = useTranslation('global');
+  return (
+    <div>
+      {t('header.chooseLanguage')}
+      <button onClick={() => i18n.changeLanguage('en')}>EN</button>
+      <button onClick={() => i18n.changeLanguage('np')}>ES</button>
+    </div>
+  );
 }
 
 export default App;
