9bbb7a8453ba27f47d3fcd4af097ae75b0bfd57d
Hypekage <jmestmnag@gmail.com>
2023-09-17 20:23:13 +0545

    initialising `i18next` with its resources and other config

--- apps/react-monorepo-practice/src/main.tsx
+++ apps/react-monorepo-practice/src/main.tsx
@@ -1,13 +1,33 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 
 import App from './app/app';
+import global_en from '../public/locales/en/global.json';
+import global_np from '../public/locales/np/global.json';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'auto',
+  fallbackLng: 'en',
+  resources: {
+    en: {
+      global: global_en
+    },
+    np: {
+      global: global_np
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </StrictMode>
 );
