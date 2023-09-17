3f9b5d3721a0a408214e83d13709ce0da0f2fc63
Hypekage <jmestmnag@gmail.com>
2023-09-17 19:54:10 +0545

    `extracting` types from `zod` object

--- libs/client/components/form/src/schema/index.ts
+++ libs/client/components/form/src/schema/index.ts
@@ -1,11 +1,19 @@
 import { z } from 'zod';
 
-const EMAIL_REQUIRED_MESSAGE = 'username field is required';
-const PASSWORD_REQUIRED_MESSAGE = 'password field is required';
+const FIRSTNAME_REQUIRED_MESSAGE = 'First name is required';
+const LASTNAME_REQUIRED_MESSAGE = 'Last name is required';
+const PHONE_REQUIRED_MESSAGE = 'Phone Number is required';
+const EMAIL_REQUIRED_MESSAGE = 'Email is required';
+const FATHER_REQUIRED_MESSAGE = 'Father Name is required';
+const FATHERNO_REQUIRED_MESSAGE = 'Father Number is required';
 
 export const formSchema = z.object({
-  email: z.string().nonempty({ message: EMAIL_REQUIRED_MESSAGE }),
-  password: z.string().nonempty({ message: PASSWORD_REQUIRED_MESSAGE }),
+  firstName: z.string().min(1, { message: FIRSTNAME_REQUIRED_MESSAGE }),
+  lastName: z.string().min(1, { message: LASTNAME_REQUIRED_MESSAGE }),
+  email: z.string().min(1, { message: EMAIL_REQUIRED_MESSAGE }),
+  phoneNo: z.string().min(1, { message: PHONE_REQUIRED_MESSAGE }),
+  fatherName: z.string().nonempty({ message: FATHER_REQUIRED_MESSAGE }),
+  fatherNo: z.string().min(1, { message: FATHERNO_REQUIRED_MESSAGE }),
 });
 
 // extracting type from formSchema
