7ad9855eacb49fdf89f8adf9b3d463ed532651f1
Hypekage <jmestmnag@gmail.com>
2023-09-21 00:17:34 +0545

    `navbar` ui `structure and responsiveness` checked.

--- apps/react-monorepo-practice/src/app/app.tsx
+++ apps/react-monorepo-practice/src/app/app.tsx
@@ -1,15 +1,7 @@
-import { useTranslation } from 'react-i18next';
+import { Navbar } from '@org/client//layouts';
 import './app.module.css';
-import { Form } from '@org/client/components/form';
 export function App() {
-  const { t, i18n } = useTranslation('global');
-  return (
-    <div>
-      {t('header.chooseLanguage')}
-      <button onClick={() => i18n.changeLanguage('en')}>EN</button>
-      <button onClick={() => i18n.changeLanguage('np')}>ES</button>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
