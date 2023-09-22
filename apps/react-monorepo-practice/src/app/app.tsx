b23636dfaa62e2a1ff93421df1f02a9f7cae22e2
Hypekage <jmestmnag@gmail.com>
2023-09-22 13:28:49 +0545

    `zodForm` successfully worked.

--- apps/react-monorepo-practice/src/app/app.tsx
+++ apps/react-monorepo-practice/src/app/app.tsx
@@ -1,7 +1,14 @@
 import { Navbar } from '@org/client//layouts';
 import './app.module.css';
+import { Form } from '@org/client/components/form';
+import { BusinessUnit } from '@org/client/pages';
+
 export function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <BusinessUnit />
+    </div>
+  );
 }
 
 export default App;
